af0a9b896c11784dab28e40b79fac243d1c12a12
Gav <gavin.mccormack@gmail.com>
2017-08-23 18:40:24 +0100

    Removed and moved extraneous functions in mk app

--- markov_functions/mk_functions.py
+++ markov_functions/mk_functions.py
@@ -10,35 +10,10 @@ import datetime
 import random
 import importlib
 import codecs
+from dj import development_tools  as log
 
 importlib.reload(sys)  
-nltk.data.path.append(os.path.join(os.path.dirname(__file__), 'nltk_data'))
-
-MARKOV_LOGGING = False # Hook this up to logging function 
-
-def PrintException():
-    exc_type, exc_obj, tb = sys.exc_info()
-    f = tb.tb_frame
-    lineno = tb.tb_lineno
-    filename = f.f_code.co_filename
-    linecache.checkcache(filename)
-    line = linecache.getline(filename, lineno, f.f_globals)
-    return 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
-
-
-
-def g_log_exception(err, filename="general_log.txt", exception=False):
-  if MARKOV_LOGGING:
-    errorFile = open(os.path.join(os.path.dirname(__file__), 'errors', filename), "a")
-    startTime = time.time()
-    startTime = datetime.datetime.fromtimestamp(startTime) # check 
-    errorFile.write("Time: " + str(startTime) + "  ")
-    errorFile.write(str(err))
-    if exception:
-      errorFile.write("\n" + PrintException())
-    errorFile.write("\n")
-    errorFile.close()
-
+nltk.data.path.append(os.path.join(os.path.dirname(__file__), 'nltk_data')) ## Add NLTK grammars & tagging
 
 ## Experimental and humourin' modules
 
@@ -54,7 +29,7 @@ def tag_check(tag):
   if tag[1] in plural:
     return random.choice(gross_words_list_p) + "::"+tag[1]
   else:
-    g_log_exception(tag,"NLTK_words.txt")
+    log.g_log_exception(tag,"NLTK_words.txt")
     return "::".join(tag)
 
   # NNP, NN
@@ -76,12 +51,12 @@ class POSifiedText(markovify.Text):
           try:
             words_cache.append( tag_check(tag) )
           except Exception as e:
-            g_log_exception("POSifiedText Problem" ,"NLTK_words.txt",exception=True)
+            log.g_log_exception("POSifiedText Problem" ,"NLTK_words.txt",exception=True)
 
       words = words_cache
       return words
     except Exception as e:
-      g_log_exception("Test P Except", exception=True)
+      log.g_log_exception("Test P Except", exception=True)
       return words
 
   def word_join(self, words):
@@ -96,29 +71,29 @@ class POSifiedText(markovify.Text):
 
 def build_model(text, conf_state_size, posEnabled=False):
   if posEnabled:
-    g_log_exception("We used fancy piece of shit text")
+    log.g_log_exception("We used fancy piece of shit text")
     return (POSifiedText(text, state_size=conf_state_size))
   else:
-    g_log_exception("We used good old simple markov chain")
+    log.g_log_exception("We used good old simple markov chain")
     return (markovify.Text(text, state_size=conf_state_size))
 
 def load_book(filesize, filename="ulysses.txt"):
   try:
-    g_log_exception("Begin book load")
+    log.g_log_exception("Begin book load")
     path = os.path.join(os.path.dirname(__file__), 'books', filename)
-    g_log_exception("Booksize: " + str(filesize) + " / Filename: " + filename)
+    log.g_log_exception("Booksize: " + str(filesize) + " / Filename: " + filename)
     with codecs.open(path, "r", encoding='utf-8') as file:    # Codec module to avoid ascii encode/decode errors 
       bookText = [next(file) for x in range(filesize)]
     bookText = " ".join(bookText).strip()
     bookText = bookText.encode('utf8')
-    g_log_exception("Book load ended")
+    log.g_log_exception("Book load ended")
     return bookText
   except:
-    g_log_exception("Book load failed for the following reason:")
-    g_log_exception(PrintException())
+    log.g_log_exception("Book load failed for the following reason:")
+    log.g_log_exception(log.PrintException())
 
 def markovify_text(text, lines, ulysses, erotic, posEnabled, conf_state_size=2 ):
-  g_log_exception("Begin markovification")
+  log.g_log_exception("Begin markovification")
   try: 
     text = text + load_book( int(ulysses) )
     text = text + load_book( int(erotic) , "megarotic.txt")
@@ -132,11 +107,11 @@ def markovify_text(text, lines, ulysses, erotic, posEnabled, conf_state_size=2 )
           output = output + "<br />"
         output = output + sentence 
     #output = changePronouns(output)
-    g_log_exception("End markovification")
+    log.g_log_exception("End markovification")
     return output
   except:
-    g_log_exception("Generating text failed for the following reason:")
-    g_log_exception(PrintException())
+    log.g_log_exception("Generating text failed for the following reason:")
+    log.g_log_exception(log.PrintException())
     return "Failed to generate any text : ("
 
 
@@ -146,45 +121,3 @@ def markovify_sentence(text_model): # Extended the markov method to fail silentl
     return sentence.unicode()
   return "[~Could not find a match~]" ## Not sure what this is doing here anymore - too many revisions.
 
-
-
-
-
-
-## To do
-### - Clean up code. It's shit
-### Redo sentences if they provide a None result
-### Parse ASCII characters awaaay! awaay with them!
-def markovify_whatsapp(text,lines):
-  poname = 'Bob: '
-  ptname = 'Sally: '
-  pot = "1:"
-  ptt = "2:"
-  processedText = ""
-  file_i = 0
-  try:
-    for line in text.splitlines():
-      line = line.encode('utf-8')
-      if not line:
-        i = line.find(':')
-        line = line[i+5:]
-        if (line.find('Hannah') > 0):
-          pot = pot + (line.split(':')[1]) + '\n'
-        elif (line.find('Wisearse') > 0):
-          ptt = ptt + (line.split(':')[1]) + '\n'
-        if i == -1:
-          pass
-  except Exception as e:
-    return "fuck" + str(e)
-  try:
-    pot = markovify.NewlineText(pot, state_size=1)
-    ptt = markovify.NewlineText(ptt, state_size=1)
-    for n in range(int(lines)):
-      processedText = processedText + "<br/>" + poname  + markovify_sentence(pot)
-      processedText = processedText + "<br/>" + ptname  + markovify_sentence(ptt)
-  except Exception as e:
-    return "Second: " + str(e)
-  return processedText
-
-def test():
-  return "Y"
