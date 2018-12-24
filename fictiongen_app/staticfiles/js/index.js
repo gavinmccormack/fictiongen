5a49dafd32b107b6078fa0084566372622d396c9
root <root@DESKTOP-ED8BBBR.localdomain>
2018-12-24 15:45:35 +0000

    React side, pre merge

--- fictiongen_app/staticfiles/js/index.js
+++ fictiongen_app/staticfiles/js/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-var data = require('./data.js');
-
-console.log(data);
+const DataInterface = require('./data.js');
+var data = new DataInterface;
 // Settings; to be hooked into room settings / etc but for now can be per session 
-const settingsArea = <inputArea><input class='checkbox' id='grammar-entry' type='checkbox' /><span></span><label>Label Of Thing</label></inputArea>;
+const settingsArea = <h1></h1>//<inputArea><input class='checkbox' id='grammar-entry' type='checkbox' /><span></span><label>Label Of Thing</label></inputArea>;
 
 ReactDOM.render(
     settingsArea, 
@@ -15,17 +14,22 @@ ReactDOM.render(
 class StoryPanel extends React.Component {
     constructor(props){
         super(props)
+        this.storyLines = ["Some placeholder", "text until", "later"]; // Array of strings
     }
 
     render(){
-        this.update();
-        return (<h1>basic render enabled</h1>);
+        // A div wrapper with an element wrapper for each line of text. 
+        // These will have element ID's and other information, but this is a basic outline
+        var storyData = this.update();
+        const lineFormat = <p class="mk-line">{content}</p>;
+        var lines = this.storyLines.map((line) => { return <span class="mk-line">{line} </span>;});
+        console.log(lines);
+        return (<div class="story-content"><p>{lines}</p></div>);
     }
 
     update(){
-        console.log(data);
-        storyData = data.getUpdatedStory();
-        console.log(storyData);
+        var storyData = data.getUpdatedStory();
+        return storyData; 
     }
 }
 
