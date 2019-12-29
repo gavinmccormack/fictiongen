e7146110a92ab24abb57d9cb3c259f6a194030fe
TheGrimJam <gavin.mccormack@gmail.com>
2019-12-29 23:21:17 +0000

    Add venv location to wsgi + wsgi script tweaks + gitignore for venv

--- fictiongen_app/core/wsgi.py
+++ fictiongen_app/core/wsgi.py
@@ -6,7 +6,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
@@ -15,7 +14,6 @@ from django.core.wsgi import get_wsgi_application
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fictiongen_app.core.settings")
 
 application = get_wsgi_application()
